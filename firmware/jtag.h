a7de68321482d2af6f48fc69b57cbbc321986e26
Wojciech M. Zabołotny <wzab01@gmail.com>
2025-05-08 12:06:55 +0200

    Changed pin definitions for RP2040-Zero

--- firmware/jtag.h
+++ firmware/jtag.h
@@ -28,14 +28,18 @@
  */
 void cmd_handle(uint8_t* rxbuf, uint32_t count, uint8_t* tx_buf);
 
-static int tdi_gpio = 16;
-static int tdo_gpio = 17;
-static int tck_gpio = 18;
-static int tms_gpio = 19;
+//static int tdi_gpio = 27;
+//static int tdo_gpio = 28;
+//static int tck_gpio = 29;
+//static int tms_gpio = 26;
+static int tdi_gpio = 26;
+static int tdo_gpio = 27;
+static int tck_gpio = 28;
+static int tms_gpio = 29;
 
 #define JTAG_ITF     1
 
-#define LED_PIN      25
+#define LED_PIN      16
 
 // How does this 'feature' even work? Perhaps the 'slew rate' on Raspberry Pi
 // GPIO pins (not Pico?) is slow enough to require these delays? Or the "GPIO
